329c7fc3511d6f221116ef6731f7f816c8d75648
Luckshay <luckshay.paul2002@gmail.com>
2023-03-05 21:24:28 +0530

    Working on Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,21 @@
+import {FaBars, FaTimes} from "react-icons/fa"
 function Navbar(){
     return (
         <header>
             <h3>HealthPlus</h3>
             <nav>
                 <a href="/#">Home</a>
-                
+                <a href="/#">About Us</a>
+                <a href="/#">Contact Us</a>
+                <a href="/#">Privacy Policies</a>
+                <a href="/#">Login</a>
+                <button>
+                    <FaTimes/>
+                </button>
             </nav>
+            <button>
+                <FaBars/>
+            </button>
         </header>
     );
 }
