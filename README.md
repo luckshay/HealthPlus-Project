cfc1bdd0237cd732d5c76912378fde72c24e5a8f
Luckshay <luckshay.paul2002@gmail.com>
2023-03-05 18:43:32 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HealthPlus-Project
+Liza Connected
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+    <h1>Hi, This is for testing. This is Version 2.</h1>
+    </div>
+  );
+}
+
+export default App;
