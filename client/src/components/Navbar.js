df35edc612b80dfdfb8ce02ade291a62f9e03384
Luckshay <luckshay.paul2002@gmail.com>
2023-03-07 23:10:53 +0530

    altered the header CSS

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,12 +11,13 @@ function Navbar(){
             {/* <img src={require("../../src/assets/logo/Health-Plus Logo 2.png")} width={75} height={75} alt=""></img> */}
             {/* client\src\assets\logo\Health-Plus Logo 2.png */}
             
-            <h3>HealthPlus</h3>
+            <h2>HealthPlus</h2>
             <nav ref={navRef}>
+                
                 <a href="/#">Home</a>
                 <a href="/#">About Us</a>
                 <a href="/#">Contact Us</a>
-                <a href="/#">Privacy Policies</a> 
+                <a href="/#">Policies</a> 
                 <a href="/#">Login</a>
                 <button className="nav-btn nav-close-btn"onClick={showNavbar}>
                     <FaTimes/>
