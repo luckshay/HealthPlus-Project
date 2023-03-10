caecdda2e12a38af2c5e47df17c5dd1bd6eb4fdd
LIZA1608 <liza1608.be20@chitkara.edu.in>
2023-03-10 19:11:04 +0530

    home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/home.css';
 
-const Home = () => {
+function Home() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabClick = (tabNumber) => {
+    setActiveTab(tabNumber);
+  };
+
+  const tabContent = [
+    {
+      id: 1,
+      title: 'Paitent Registration Number',
+      image: '/image1.jpg',
+      content: 'Tab 1 Content',
+    },
+    {
+      id: 2,
+      title: 'Health Facility Registration',
+      image: '/image2.jpg',
+      content: 'Tab 2 Content',
+    },
+    {
+      id: 3,
+      title: 'HealthCare Professional Registration ',
+      image: '/image3.jpg',
+      content: 'Tab 3 Content',
+    },
+    {
+      id: 4,
+      title: 'Blood Donation ',
+      image: '/image4.jpg',
+      content: 'Tab  Content',
+    },
+  ];
+
   return (
-    <div>Home</div>
-  )
+    <>
+    <div className="container">
+      <div className="top-section">
+        {tabContent.map((tab) => (
+          <div
+            key={tab.id}
+            className={`tab ${activeTab === tab.id ? 'active' : ''}`}
+            onClick={() => handleTabClick(tab.id)}
+          >
+            <img src={tab.image} alt={tab.title} />
+            <h3>{tab.title}</h3>
+          </div>
+        ))}
+      </div>
+      <div className="bottom-section">
+        {tabContent.map((tab) => (
+          <div
+            key={tab.id}
+            className={`content ${activeTab === tab.id ? 'active' : ''}`}
+          >
+            <h2>{tab.content}</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
