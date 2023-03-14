9b50abdb42b52cf467a1451c26c057dc9c3fc367
Luckshay <luckshay.paul2002@gmail.com>
2023-03-14 23:20:20 +0530

    Removed an Inconsistency

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // const DB_URI = 'mongodb://localhost:27017/testuser';
 // const DB_URI = 'mongodb+srv://admin:dWB2NWV88lptI3tW@healthplus.evvargk.mongodb.net/?retryWrites=true&w=majority';
-const DB_URI = `${process.env.MONGO_URI}`;
+const DB_URI = process.env.MONGO_URI;
 
 
 const connectDB = async () => {
