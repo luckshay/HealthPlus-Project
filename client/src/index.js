29088b7bd0ddc61c83ed2255021a9f1b7b2d5e18
Luckshay <luckshay.paul2002@gmail.com>
2023-03-23 16:00:25 +0530

    removed an inconsistency

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMugHot } from '@fortawesome/free-solid-svg-icons';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
