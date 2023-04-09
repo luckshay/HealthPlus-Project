76b5751484ba88d58622c46e712d3d6873c56bed
Luckshay <luckshay.paul2002@gmail.com>
2023-04-09 21:11:19 +0530

    resolved an inconsistency

--- client/src/components/pages/Contactpage.js
+++ client/src/components/pages/Contactpage.js
@@ -101,6 +101,7 @@ const ContactPage = (props) => {
         <h4>Map</h4>
         <div className="map-container">
         <iframe
+        title="locstion"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3437.1751370296724!2d76.65529312608415!3d30.51608642322264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390fc32344a6e2d7%3A0x81b346dee91799ca!2sChitkara%20University!5e0!3m2!1sen!2sin!4v1680468676794!5m2!1sen!2sin"
         width="100%"
         height="90%"
